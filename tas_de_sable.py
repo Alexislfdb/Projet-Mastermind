3fe56516f30faefc58ab5f63c92c4e802919ff32
Alexis LE FORESTIER <alexislf120202@gmail.com>
2022-02-08 12:21:03 +0100

    aha

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,17 +1,21 @@
 import tkinter
 import random
-a = 5
-configuration_matrice = []
+
 
 
 def configuration(n):
     ligne_configuration = []
+    configuration_matrice = []
     for i in range(n):
         for j in range(n):
             ligne_configuration.append(random.randint(0,9))
         configuration_matrice.append(ligne_configuration)
         ligne_configuration = []
-    print(configuration_matrice)
+    for a in range(len(configuration_matrice)):
+        print(configuration_matrice[a], "\n")
+    
+    return configuration_matrice
+        
 
 configuration(3)
 
